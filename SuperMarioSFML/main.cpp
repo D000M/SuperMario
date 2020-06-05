187fc9998c1381edf6c1138cfa42e5799824c860
D000M <georgislawow@gmail.com>
2020-06-05 11:17:28 +0300

    Initial for Super Mario SFML
    First commit

---
+++ SuperMarioSFML/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: D000M
+ *
+ * Created on June 5, 2020, 11:13 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    auto a = 5;
+    std::cout << a << std::endl;
+    
+    return 0;
+}
+
